0d8c56e512e9f65c8c07e36e738bf4197fe0f63c
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-06 22:16:16 +0600

    "working on navbar ui"

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../../pages/Footer";
-import Navbar from "./Navbar";
+
 import NavbarPro from "./NavbarPro";
 
 const MainLayout = () => {
   return (
     <div className="">
       <NavbarPro />
-      <Navbar />
       <Outlet />
       <Footer />
     </div>
--- src/components/layouts/NavbarPro.tsx
+++ src/components/layouts/NavbarPro.tsx
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
-
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { FaAlignJustify } from "react-icons/fa6";
 const NavbarPro = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleLogout = () => {
+    // Handle logout logic here
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
   };
 
   return (
@@ -19,47 +27,16 @@ const NavbarPro = () => {
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtfz4gBB6dnwQ-nq1etaQI-dTIKXoiNPheAw&usqp=CAU"
+            className="h-8 rounded-full"
             alt="Flowbite Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            Donate Ease
           </span>
         </a>
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button
-            onClick={toggleMenu}
-            type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Get started
-          </button>
-          <button
-            onClick={toggleMenu}
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-sticky"
-            aria-expanded={isMenuOpen ? "true" : "false"}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-        </div>
+
+        <FaAlignJustify className="md:hidden" onClick={toggleMenu} />
         <div
           className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
             isMenuOpen ? "block" : "hidden"
@@ -99,6 +76,27 @@ const NavbarPro = () => {
               >
                 Contact
               </a>
+            </li>
+            <li>
+              <div className="  ">
+                <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse ">
+                  {isLoggedIn ? (
+                    <button
+                      onClick={handleLogout}
+                      type="button"
+                      className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Logout
+                    </button>
+                  ) : (
+                    <NavLink to="/login">
+                      <button className="text-white rounded-xl bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium  text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Login
+                      </button>
+                    </NavLink>
+                  )}
+                </div>
+              </div>
             </li>
           </ul>
         </div>
