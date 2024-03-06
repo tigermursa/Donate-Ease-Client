701fd6c9d84b5eabc84a0b1e8ffb583afe35cc80
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-06 21:08:38 +0600

    "added framer motion animation on banner section"

--- src/pages/Banner.tsx
+++ src/pages/Banner.tsx
@@ -1,24 +1,70 @@
+import { motion } from "framer-motion";
+
 const Banner = () => {
+  const imageAnimation = {
+    hidden: { y: "-100%", opacity: 0 },
+    visible: { y: 0, opacity: 1 },
+  };
+
+  const textAnimation1 = {
+    hidden: { x: "-100%", opacity: 0 },
+    visible: { x: 0, opacity: 1 },
+  };
+  const textAnimation2 = {
+    hidden: { x: "100%", opacity: 0 },
+    visible: { x: 0, opacity: 1 },
+  };
+
+  const buttonAnimation = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center mt-10 mb-10">
+    <motion.div className="flex flex-col justify-center items-center mt-10 mb-10">
       {/* text */}
-      <div className="text-center space-y-6 mb-5" >
-        <p className="text-lg md:text-2xl font-semibold">
+      <div className="text-center space-y-6 mb-5">
+        <motion.p
+          initial="hidden"
+          animate="visible"
+          variants={textAnimation1}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="text-lg md:text-2xl font-semibold"
+        >
           Donate Ease Specially Created for
-        </p>
-        <h2 className="text-xl  md:text-5xl font-semibold">
+        </motion.p>
+        <motion.h2
+          initial="hidden"
+          animate="visible"
+          variants={textAnimation2}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="text-xl md:text-5xl font-semibold"
+        >
           Charity, Nonprofit, Fundraising & Crowdfunding
-        </h2>
-        <button className="btn-primary">Contact now</button>
+        </motion.h2>
+        <motion.button
+          variants={buttonAnimation}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 1, ease: "easeInOut", delay: 1 }} // Delay the button animation by 1 second
+          className="btn-primary"
+        >
+          Contact now
+        </motion.button>
       </div>
       {/* Hero Image */}
-      <div>
+      <motion.div
+        variants={imageAnimation}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 1, ease: "easeInOut" }}
+      >
         <img
           src="https://www.thknh.org.sg/wp-content/uploads/2021/10/Donation-in-kind-1-scaled-e1635155009580.jpg"
           alt=""
         />
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
