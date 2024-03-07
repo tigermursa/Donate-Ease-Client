65b5edb878e1deb16923470ebf04c6ca0326b931
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-07 11:34:05 +0600

    "added dashboard private button on the navbar"

--- src/components/layouts/NavbarPro.tsx
+++ src/components/layouts/NavbarPro.tsx
@@ -19,7 +19,9 @@ const NavbarPro = () => {
 
   return (
     <nav
-      className={`  ${isDarkMode? "  bg-gray-500 text-white": "bg-white text-black"} fixed w-full z-20 top-0 start-0 border-b border-gray-500 dark:border-gray-600 ${
+      className={`  ${
+        isDarkMode ? "  bg-gray-500 text-white" : "bg-white text-black"
+      } fixed w-full z-20 top-0 start-0 border-b border-gray-500 dark:border-gray-600 ${
         isMenuOpen ? "open" : ""
       }`}
     >
@@ -43,7 +45,6 @@ const NavbarPro = () => {
           id="navbar-sticky"
         >
           <div>
-            
             <ul className=" cursor-pointer flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  md:bg-opacity-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700   ">
               <li>
                 <Link
@@ -90,6 +91,19 @@ const NavbarPro = () => {
                   Contact
                 </Link>
               </li>
+              {isLoggedIn ? (
+                <li>
+                  <NavLink
+                    to="/dashboard"
+                    className="block py-2 px-3    rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    Dashboard
+                  </NavLink>
+                </li>
+              ) : (
+                ""
+              )}
+
               <li>
                 <button
                   onClick={toggleDarkMode}
--- src/pages/TopDonations.tsx
+++ src/pages/TopDonations.tsx
@@ -7,7 +7,7 @@ const TopDonations = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 1,
     responsive: [
       {
@@ -20,13 +20,30 @@ const TopDonations = () => {
     ],
   };
 
+  const data = [
+    {
+      img: "https://assets.website-files.com/63904f663019b0d8edf8d57c/63905435069fb009d43abbb1_Ellipse-2.jpg",
+      name: "Mursalin Hossain",
+      profession: "Web Developer",
+      comment:
+        "I truly believe that helping others is another way to helping myself, I feel so inner peace inside my heart by donating",
+    },
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUjIBODT86UZLa3z-Yiw2_ZIDog3HDWOUIfg&usqp=CAU",
+      name: "Finona jesuja",
+      profession: "CEO Ant ",
+      comment:
+        "I truly believe that helping others is another way to helping myself, I feel so inner peace inside my heart by donating",
+    },
+  ];
+
   return (
     <div className="mx-auto">
       <section className="relative">
         <div className="mx-auto max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-32">
           <div className="mx-auto w-full max-w-3xl text-center">
             <h2 className="text-3xl font-semibold md:text-5xl">
-              Top 6 Donations
+              Top Donations
             </h2>
             <div className="mx-auto mb-8 mt-4 max-w-[528px] md:mb-12 lg:mb-16">
               <p className="text-[#636262]">
@@ -37,76 +54,26 @@ const TopDonations = () => {
           </div>
 
           <Slider {...settings}>
-            {/* Card 1 */}
-            <div
-              key={1}
-              className="relative mb-4 flex flex-col items-center justify-between gap-6 rounded-2xl border border-solid border-[#636262] bg-white px-8 pb-8 pt-16 max-[767px]:mt-4 md:mb-8 md:px-6 md:pb-8 md:pt-16 lg:mb-4 mt-10"
-            >
-              <div className="mb-4 flex flex-col items-center">
-                <h6 className="text-gray-500 font-semibold">John Doe</h6>
-                <p className="text-sm text-[#636262]">Designer</p>
+            {data.map((card, index) => (
+              <div
+                key={index}
+                className="cursor-grab relative mb-4 flex flex-col items-center justify-between gap-6 rounded-2xl border border-solid border-[#636262] bg-white px-8 pb-8 pt-16 max-[767px]:mt-4 md:mb-8 md:px-6 md:pb-8 md:pt-16 lg:mb-4 mt-10"
+              >
+                <div className="mb-4 flex flex-col items-center">
+                  <h6 className="text-gray-500 font-semibold">{card.name}</h6>
+                  <p className="text-sm text-[#636262]">{card.profession}</p>
+                </div>
+                <p className="mb-4 text-[#636262]">{card.comment}</p>
+                <div className="flex">{/* Add your rating icons here */}</div>
+                <div className="absolute bottom-[auto] left-[auto] right-[50%] top-[-32px] flex h-16 w-16 translate-x-1/2 flex-col items-center justify-center rounded-full border border-solid border-[#9b9b9b] bg-white shadow-md mt-4 md:mt-0">
+                  <img
+                    src={card.img}
+                    alt=""
+                    className="inline-block h-full w-full rounded-full"
+                  />
+                </div>
               </div>
-              <p className="mb-4 text-[#636262]">
-                “Lorem ipsum dolor sit amet, elit ut aliquam, purus sit amet
-                luctus venenatis elit ut aliquam, purus sit amet luctus
-                venenatis"
-              </p>
-              <div className="flex">{/* Add your rating icons here */}</div>
-              <div className="absolute bottom-[auto] left-[auto] right-[50%] top-[-32px] flex h-16 w-16 translate-x-1/2 flex-col items-center justify-center rounded-full border border-solid border-[#9b9b9b] bg-white shadow-md mt-4 md:mt-0">
-                <img
-                  src="https://assets.website-files.com/63904f663019b0d8edf8d57c/63905435069fb009d43abbb1_Ellipse-2.jpg"
-                  alt=""
-                  className="inline-block h-full w-full rounded-full"
-                />
-              </div>
-            </div>
-            {/* Card 1 */}
-            <div
-              key={1}
-              className="relative mb-4 flex flex-col items-center justify-between gap-6 rounded-2xl border border-solid border-[#636262] bg-white px-8 pb-8 pt-16 max-[767px]:mt-4 md:mb-8 md:px-6 md:pb-8 md:pt-16 lg:mb-4 mt-10"
-            >
-              <div className="mb-4 flex flex-col items-center">
-                <h6 className="text-gray-500 font-semibold">John Doe</h6>
-                <p className="text-sm text-[#636262]">Designer</p>
-              </div>
-              <p className="mb-4 text-[#636262]">
-                “Lorem ipsum dolor sit amet, elit ut aliquam, purus sit amet
-                luctus venenatis elit ut aliquam, purus sit amet luctus
-                venenatis"
-              </p>
-              <div className="flex">{/* Add your rating icons here */}</div>
-              <div className="absolute bottom-[auto] left-[auto] right-[50%] top-[-32px] flex h-16 w-16 translate-x-1/2 flex-col items-center justify-center rounded-full border border-solid border-[#9b9b9b] bg-white shadow-md mt-4 md:mt-0">
-                <img
-                  src="https://assets.website-files.com/63904f663019b0d8edf8d57c/63905435069fb009d43abbb1_Ellipse-2.jpg"
-                  alt=""
-                  className="inline-block h-full w-full rounded-full"
-                />
-              </div>
-            </div>
-            {/* Card 1 */}
-            <div
-              key={1}
-              className="relative mb-4 flex flex-col items-center justify-between gap-6 rounded-2xl border border-solid border-[#636262] bg-white px-8 pb-8 pt-16 max-[767px]:mt-4 md:mb-8 md:px-6 md:pb-8 md:pt-16 lg:mb-4 mt-10"
-            >
-              <div className="mb-4 flex flex-col items-center">
-                <h6 className="text-gray-500 font-semibold">John Doe</h6>
-                <p className="text-sm text-[#636262]">Designer</p>
-              </div>
-              <p className="mb-4 text-[#636262]">
-                “Lorem ipsum dolor sit amet, elit ut aliquam, purus sit amet
-                luctus venenatis elit ut aliquam, purus sit amet luctus
-                venenatis"
-              </p>
-              <div className="flex">{/* Add your rating icons here */}</div>
-              <div className="absolute bottom-[auto] left-[auto] right-[50%] top-[-32px] flex h-16 w-16 translate-x-1/2 flex-col items-center justify-center rounded-full border border-solid border-[#9b9b9b] bg-white shadow-md mt-4 md:mt-0">
-                <img
-                  src="https://assets.website-files.com/63904f663019b0d8edf8d57c/63905435069fb009d43abbb1_Ellipse-2.jpg"
-                  alt=""
-                  className="inline-block h-full w-full rounded-full"
-                />
-              </div>
-            </div>
-            {/* Add more cards as needed */}
+            ))}
           </Slider>
         </div>
 
